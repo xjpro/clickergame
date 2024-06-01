2727eec6bd38667d595eefdbc49240f931080d98
Joshua Prodahl <jprodahl@gmail.com>
2024-06-01 10:09:00 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,16 @@
+
+
+let greeting = 'hello'
+const users = ["Leon", "Silas"]
+
+greeting = "josh"
+users.forEach(user => {
+	console.log(greeting + " " + user)
+})
+
+
+let n = 3 + 2;
+
+console.log(n)
+
+
