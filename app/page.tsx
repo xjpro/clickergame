c8252160e5ca774fe111df4df42f762627abfbd8
Joshua Prodahl <jprodahl@gmail.com>
2024-06-01 11:06:35 -0500

    fix bugs with rotation

--- app/page.tsx
+++ app/page.tsx
@@ -11,30 +11,40 @@ export default function Home() {
   const [y, setY] = useState(
     typeof window !== "undefined" ? window.innerHeight / 2 : 100,
   );
+  const [rotation, setRotation ] = useState(0)
+  const [speed, setSpeed] = useState(4000);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (!clickTargetRef.current) return;
-      const width = clickTargetRef.current.clientWidth;
 
-      const height = clickTargetRef.current.clientHeight;
-      setX(Math.max(0, Math.random() * window.innerWidth - width));
-      setY(Math.max(0, Math.random() * window.innerHeight - height));
-    }, 2000);
+    let timeout: any;
+    function movePizza() {
+      if (clickTargetRef.current) {
+        const width = clickTargetRef.current.clientWidth;
+        const height = clickTargetRef.current.clientHeight;
+        setX(Math.max(0, Math.random() * window.innerWidth - width));
+        setY(Math.max(0, Math.random() * window.innerHeight - height));
+        setRotation(Math.random() * 360);
+      }
+
+      timeout = setTimeout(movePizza, speed);
+    }
+
+    movePizza();
 
     return () => {
-      if (interval) {
-        clearInterval(interval);
+      if (timeout) {
+        clearTimeout(timeout);
       }
     };
-  }, []);
+  }, [clickTargetRef]);
 
   function clicked(event: React.MouseEvent<HTMLDivElement>) {
     setScore((score) => score + 1);
+    // setSpeed((speed) => speed * 0.95);
   }
 
   const style = {
-    transform: `translate(${x}px, ${y}px) rotate(${Math.random() * 360}deg)`,
+    transform: `translate(${x}px, ${y}px) rotate(${rotation}deg)`,
   };
 
   return (
